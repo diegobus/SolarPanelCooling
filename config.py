d63dae55fc48ef88d1346ef9df72afc7b4d2933d
Diego Bustamante <diegobus@stanford.edu>
2025-03-11 18:29:55 -0700

    Delete config.py with API key

--- config.py
+++
@@ -1,8 +0,0 @@
-# NREL API Configuration
-# This file contains sensitive information and should not be committed to version control
-
-# Your NREL API Key
-API_KEY = "dpBD04Vn13sHyLsZXwDIWS2Nuhu1oTewSTHiv6s5"
-
-# Your Email for NREL API
-EMAIL = "diegobus@stanford.edu"
